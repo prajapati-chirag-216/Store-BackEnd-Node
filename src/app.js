168c5dd3f0e35652bebf6332694a90a9a58108a5
prajapati-chirag-216 <prajapatichirag2162003@gmail.com>
2024-06-24 14:52:32 +0530

    added new port

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5000"],
+    origin: ["http://localhost:5000", "http://localhost:4200"],
     credentials: true,
   })
 );
